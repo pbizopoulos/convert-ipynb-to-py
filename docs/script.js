e484c85f4d6762119304907f409735cb2d5f4e77
Paschalis Bizopoulos <pbizopoulos@protonmail.com>
2023-04-13 14:20:20 +0300

    .

--- docs/script.js
+++ docs/script.js
@@ -1,26 +1,40 @@
 "use strict";
 const loadIpynbFileInputFile = document.getElementById("load-ipynb-file-input-file");
+const loadIpynbFileInputUrl = document.getElementById("load-ipynb-file-input-url");
 loadIpynbFileInputFile.onchange = loadIpynbFileInputFileOnChange;
+loadIpynbFileInputUrl.onchange = loadIpynbFileInputUrlOnChange;
+
+function ipynbToScript(data) {
+	const cellSourceArray = [];
+	const jsonParsed = JSON.parse(data);
+	for (const cell of jsonParsed.cells) {
+		let cellSource = "";
+		if (cell.cell_type === "code") {
+			cellSource = cell.source.join("");
+		} else if (cell.cell_type === "markdown") {
+			cellSource = cell.source.map((element) => `# ${element}`).join("");
+		}
+		cellSourceArray.push(`${cellSource}\n`);
+	}
+	const output = cellSourceArray.join("");
+	saveData([output], "main.py");
+}
 
 function loadIpynbFileInputFileOnChange() {
-	const file = document.getElementById("load-ipynb-file-input-file").files[0];
+	const file = loadIpynbFileInputFile.files[0];
 	const fileReader = new FileReader();
 	fileReader.readAsText(file);
 	fileReader.onload = (event) => {
-		const cellSourceArray = [];
-		const jsonParsed = JSON.parse(event.target.result);
-		for (const cell of jsonParsed.cells) {
-			let cellSource = "";
-			if (cell.cell_type === "code") {
-				cellSource = cell.source.join("");
-			} else if (cell.cell_type === "markdown") {
-				cellSource = cell.source.map((element) => `# ${element}`).join("");
-			}
-			cellSourceArray.push(`${cellSource}\n`);
-		}
-		const output = cellSourceArray.join("");
-		saveData([output], "main.py");
+		ipynbToScript(event.target.result);
 	};
+}
+
+function loadIpynbFileInputUrlOnChange() {
+	fetch(loadIpynbFileInputUrl.value)
+		.then((response) => response.text())
+		.then((blob) => {
+			ipynbToScript(blob);
+		});
 }
 
 function saveData(data, fileName) {
